05619fa921785d441dba652db5ed6c2f0e2a8ca3
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2023-11-02 17:12:48 -0400

    updated release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='code_wallet',
-    version='1.0.3',
+    version='1.0.4',
     packages=find_packages(),
     install_requires=open('requirements.txt').readlines(),
     long_description=long_description,
