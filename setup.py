6bf24c66c0031b0e2f148b71dfb0990f0ffecf68
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2023-11-06 16:26:08 -0500

    updated version

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='code_wallet',
-    version='1.0.5',
+    version='1.0.7',
     packages=find_packages(),
     install_requires=open('requirements.txt').readlines(),
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/code-wallet/code-sdk-python"
 )
